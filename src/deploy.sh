ad588771e3b1521a00a7d7813d43d6385a9d276e
Rohan Yadav <rohany@alumni.cmu.edu>
2019-06-24 20:00:52 -0700

    site rebuild

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,7 +2,14 @@
 
 echo "Deploying update"
 
+CWD="$(pwd)"
+
 hugo
+
+cd /Users/rohany/Documents/resume
+make
+cd $CWD
+cp /Users/rohany/Documents/resume/rohan_resume.pdf static/
 
 cp -r public/* ../
 cd ../
